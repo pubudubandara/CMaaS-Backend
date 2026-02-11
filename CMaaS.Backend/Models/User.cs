9071bc4772365c7820b0555810978db0cfe67236
pubudubandara <pubudumb@gmail.com>
2026-02-11 20:23:34 +0530

    Update the models

---
+++ CMaaS.Backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CMaaS.Backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public int TenantId { get; set; }
+
+        public UserRole Role { get; set; } = UserRole.User;
+
+        // when converting to JSON, ignore the Tenant property to avoid circular reference issues and reduce payload size
+        [JsonIgnore]
+        public Tenant? Tenant { get; set; }
+    }
+}
---
+++ CMaaS.Backend/Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CMaaS.Backend.Models
+{
+    //Use Json converter to serialize the enum as a string in JSON responses.
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum UserRole
+    {
+        Admin,
+        User,
+        Viewer
+    }
+}
